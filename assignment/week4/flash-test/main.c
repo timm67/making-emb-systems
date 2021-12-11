3bd0afef1df12349de2e447b57fd1e6a5d28b100
timm67 <75552030+timm67@users.noreply.github.com>
2021-12-10 17:40:53 -0700

    adding main and Makefile

---
+++ assignment/week4/flash-test/main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <malloc.h>
+
+#include "spi_flash.h"
+#include "unity.h"
+
+int main(int argc, char *argv[])
+{
+	// TODO: testing here
+	return 0;
+}
